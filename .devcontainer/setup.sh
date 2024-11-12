b0a2232bb8deb374f9fede22b5d4b8a2477009ec
Doru Blanzeanu <blanzeanu.doru@protonmail.com>
2024-11-12 11:51:49 +0200

    Add working devcontainer setup
    Signed-off-by: Doru Blanzeanu <blanzeanu.doru@protonmail.com>

--- .devcontainer/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /workspaces/hyperlight
-
-# Change group ownership for the $DEVICE so that the user can access it
-# NOTE: The $USER has been added to $DEVICE_GROUP during container build time
-sudo chown -R ":$DEVICE_GROUP" $DEVICE
-
