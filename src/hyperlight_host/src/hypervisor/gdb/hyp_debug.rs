247abe6279fd948ed79f39dbbd6e0fb07071f102
Doru Blânzeanu <dblnz@pm.me>
2025-03-03 13:28:26 +0200

    move hardware breakpoints functionality to separate file
    - also add a trait that contains all the needed methods for interacting
    with a vCPU
    
    Signed-off-by: Doru Blânzeanu <dblnz@pm.me>

--- src/hyperlight_host/src/hypervisor/gdb/hyp_debug.rs
+++ src/hyperlight_host/src/hypervisor/gdb/hyp_debug.rs
@@ -14,12 +14,28 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+use crate::{HyperlightError, Result};
+
 /// Software Breakpoint size in memory
 pub const SW_BP_SIZE: usize = 1;
 /// Software Breakpoint opcode
 const SW_BP_OP: u8 = 0xCC;
 /// Software Breakpoint written to memory
 pub const SW_BP: [u8; SW_BP_SIZE] = [SW_BP_OP];
+
+/// This trait is used to define a common way of interacting with a vCPU to
+/// allow debugging functionality
+pub trait GuestVcpuDebug {
+    /// Type that wraps the vCPU functionality
+    type Vcpu;
+
+    /// Adds hardware breakpoint
+    fn add_hw_breakpoint(&mut self, vcpu_fd: &Self::Vcpu, addr: u64) -> Result<bool>;
+    /// Removes hardware breakpoint
+    fn remove_hw_breakpoint(&mut self, vcpu_fd: &Self::Vcpu, addr: u64) -> Result<bool>;
+    /// Enables or disables stepping and sets the vCPU debug configuration
+    fn set_single_step(&mut self, vcpu_fd: &Self::Vcpu, enable: bool) -> Result<()>;
+}
 
 #[cfg(kvm)]
 pub mod kvm {
@@ -76,7 +92,7 @@ pub mod kvm {
         /// The 7th debug register is used to enable the breakpoints
         /// For more information see: DEBUG REGISTERS chapter in the architecture
         /// manual
-        pub fn set_debug_config(&mut self, vcpu_fd: &VcpuFd, step: bool) -> Result<()> {
+        fn set_debug_config(&mut self, vcpu_fd: &VcpuFd, step: bool) -> Result<()> {
             let addrs = &self.hw_breakpoints;
 
             self.dbg_cfg.arch.debugreg = [0; 8];
@@ -108,7 +124,7 @@ pub mod kvm {
         }
 
         /// Method that adds a breakpoint
-        pub fn add_breakpoint(&mut self, vcpu_fd: &VcpuFd, addr: u64) -> Result<bool> {
+        fn add_breakpoint(&mut self, vcpu_fd: &VcpuFd, addr: u64) -> Result<bool> {
             if self.hw_breakpoints.len() >= Self::MAX_NO_OF_HW_BP {
                 Ok(false)
             } else if self.hw_breakpoints.contains(&addr) {
@@ -122,7 +138,7 @@ pub mod kvm {
         }
 
         /// Method that removes a breakpoint
-        pub fn remove_breakpoint(&mut self, vcpu_fd: &VcpuFd, addr: u64) -> Result<bool> {
+        fn remove_breakpoint(&mut self, vcpu_fd: &VcpuFd, addr: u64) -> Result<bool> {
             if self.hw_breakpoints.contains(&addr) {
                 self.hw_breakpoints.retain(|&a| a != addr);
                 self.set_debug_config(vcpu_fd, self.single_step)?;
@@ -131,6 +147,39 @@ pub mod kvm {
             } else {
                 Ok(false)
             }
+        }
+
+        /// Translates the guest address to physical address
+        fn translate_gva(&self, vcpu_fd: &VcpuFd, gva: u64) -> Result<u64> {
+            let tr = vcpu_fd
+                .translate_gva(gva)
+                .map_err(|_| HyperlightError::TranslateGuestAddress(gva))?;
+
+            if tr.valid == 0 {
+                Err(HyperlightError::TranslateGuestAddress(gva))
+            } else {
+                Ok(tr.physical_address)
+            }
+        }
+    }
+
+    impl GuestVcpuDebug for KvmDebug {
+        type Vcpu = VcpuFd;
+
+        fn add_hw_breakpoint(&mut self, vcpu_fd: &Self::Vcpu, addr: u64) -> Result<bool> {
+            let addr = self.translate_gva(vcpu_fd, addr)?;
+
+            self.add_breakpoint(vcpu_fd, addr)
+        }
+
+        fn remove_hw_breakpoint(&mut self, vcpu_fd: &Self::Vcpu, addr: u64) -> Result<bool> {
+            let addr = self.translate_gva(vcpu_fd, addr)?;
+
+            self.remove_breakpoint(vcpu_fd, addr)
+        }
+
+        fn set_single_step(&mut self, vcpu_fd: &Self::Vcpu, enable: bool) -> Result<()> {
+            self.set_debug_config(vcpu_fd, enable)
         }
     }
 }
