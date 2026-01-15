168849fe64277dcc9a6417ac05987048d029371b
Doru Blânzeanu <dblnz@pm.me>
2026-01-15 17:13:56 +0200

    Add DAP server skeleton following gdb feature implementation
    Signed-off-by: Doru Blânzeanu <dblnz@pm.me>

--- src/hyperlight_host/src/hypervisor/mod.rs
+++ src/hyperlight_host/src/hypervisor/mod.rs
@@ -18,6 +18,10 @@ limitations under the License.
 #[cfg(gdb)]
 pub(crate) mod gdb;
 
+/// DAP (Debug Adapter Protocol) debugging support
+#[cfg(dap)]
+pub(crate) mod dap;
+
 /// Abstracts over different hypervisor register representations
 pub(crate) mod regs;
 
